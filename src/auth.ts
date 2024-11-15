fd0401c20646e2f3cc2a4e33def036adcd240051
lucasshuan <jlsrocha1@gmail.com>
2024-11-15 19:14:23 -0300

    fix: nextauth new user session pattern

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,18 @@
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
 
-const handler = NextAuth(authConfig);
+const handler = NextAuth({
+  callbacks: {
+    signIn({ user, account, profile, email, credentials }) {
+      console.log("###user", user);
+      console.log("###account", account);
+      console.log("###profile", profile);
+      console.log("###email", email);
+      console.log("###credentials", credentials);
+      return true;
+    },
+  },
+  ...authConfig,
+});
 
 export { handler as GET, handler as POST };
